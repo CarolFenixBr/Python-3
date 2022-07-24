5b1cef7027d32b96838c8874c6d6218f14d805a6
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-07-24 20:28:13 -0300

    Create Aula1.py

---
+++ Python Básico ( Lógica de Programação)/Aula1.py
@@ -0,0 +1,9 @@
+#Diferenças de C "escrever "para Python 'escrever'
+print('Linha 1')  #Ctrl D para replicar a linha
+print('Linha 2')
+print('Linha 3')
+print('Linha 4')
+print('Linha 5')
+#aspas duplas" ou simples'3x faz com que vire comentário e feche com as mesmas
+
+"""texto exemplo"""
