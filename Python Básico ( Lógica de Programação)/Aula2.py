82be68916cc9548b8bf73781ba7731cb0cf4d817
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-07-31 00:37:55 -0300

    Add files via upload

---
+++ Python Básico ( Lógica de Programação)/Aula2.py
@@ -0,0 +1,10 @@
+#print(123456)
+print('Caroline','Vargas')
+print('Caroline','Vargas',sep='-')
+print('Eu','e','Eu',sep='-', end='')
+print('Eu','e','Eu',sep='-')
+""" 
+000.000.000-00
+"""
+print('000','000','000', sep='.', end='-')
+print('00')
