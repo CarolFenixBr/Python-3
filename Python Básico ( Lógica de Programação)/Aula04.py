fe7745ce6657f3701d68cf413d9b114226081327
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-08-23 18:47:45 -0300

    Create Aula04.py

---
+++ Python Básico ( Lógica de Programação)/Aula04.py
@@ -0,0 +1,32 @@
+"""
+Tipos de dados
+str - string - textos 'Assim' "Assim"
+int - inteiro - 12345 0 -10-20-1500
+float - real/ponto flutuante - 0.1 1.5 -10.5
+bool - booleano/lógico - True/False 10==10
+"""
+print('Caroline', type('Caroline'))
+print(15, type(15))
+print(30.5, type(30.5))
+print('30.5', type('30.5'))
+print(20==20, type(20==20))
+print('C'=='c', type('C'=='c'))
+print(bool(''))
+print(bool(0))
+print(bool([]))
+print('Carol', type('Carol'), bool('Carol'))
+print('15', type('15'), type(int('15')))
+print('Carol', int('5'), float(5.02))
+print(2+5)
+print('2'+'5')
+
+#exercício
+
+#String:nome
+print('Caroline', type('Caroline'))
+#Idade: int
+print(28, type(28))
+#Altura : float
+print(1.63, type(1.63))
+#É maior de idade 10>20
+print(28 > 18, type(28 > 18))
