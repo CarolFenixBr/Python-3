702d7a0bfa5fc36b23b189ae3aeff843a9980b64
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-08-22 21:16:02 -0300

    Create Aula03.py

---
+++ Python Básico ( Lógica de Programação)/Aula03.py
@@ -0,0 +1,13 @@
+"""
+str - string
+"""
+
+print('alguma coisa')
+print("aspas duplas")
+print(123456)
+print("Essa é uma 'string' (str).")
+print('Essa é uma "string" (str).')
+print("Esse é meu \" texto\"(str).")
+print('Esse é meu \'texto\'(str).')
+print('Esse é meu \ntexto (str).')
+print(r'Esse é meu\'Aspas aqui\' (str).')
