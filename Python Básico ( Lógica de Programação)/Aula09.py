82ed70929ea097143999b5ed65c39919aa9f24a7
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-08-30 01:22:49 -0300

    Create Aula09.py

---
+++ Python Básico ( Lógica de Programação)/Aula09.py
@@ -0,0 +1,11 @@
+"""
+Entrada de dados - Aula 3
+"""
+nome = input ("Qual o seu nome? ")
+idade =input("Qual a sua idade? ")
+
+ano_nascimento = 2022-int(idade)
+
+print()
+print(f'{nome} tem {idade} anos. '
+      f'{nome} nascem em {ano_nascimento}.')
