51e9d7067e85e627ccb638d94b0d535ffe44d017
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-08-29 23:51:59 -0300

    Create Aula05.py

---
+++ Python Básico ( Lógica de Programação)/Aula05.py
@@ -0,0 +1,22 @@
+"""
+Operadores
++ adição
+- subtração
+* multiplação
+/ divisão
+// divisão inteira
+** exponenciação
+% retorna o módulo
+() alterar precedência
+"""
+print('Multiplicação *', 5*5)
+print('Adição +', 5+5)
+print('Subtração -', 15-5)
+print('Divisão /', 20/2)
+print(7*'Caroline')
+print('5'+'10')
+print('Caroline' + ' ' + 'Vargas e ela tem ' + str(28) + ' anos')
+print(2**10)
+print(10//3)
+print(10 % 3)
+print(5+10*10/2)
