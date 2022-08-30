e961410619ce1577ee81f744161b712c279c0188
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-08-30 00:52:37 -0300

    Create Aula07.py

---
+++ Python Básico ( Lógica de Programação)/Aula07.py
@@ -0,0 +1,11 @@
+nome = 'Caroline'
+idade = 28  #int
+altura= 1.63 #float
+e_maior = idade >18 #bool
+peso = 58
+imc = peso / altura **2
+
+print(nome, 'tem', idade,'anos de idade e seu imc é', imc)
+print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')
+print('{} tem {} anos de idade e seu imc é {:.2f}'.format(nome, idade, imc))
+print('{n} tem {i} anos de idade e seu imc é {im:.2f}'.format(n=nome,i= idade,im = imc))
