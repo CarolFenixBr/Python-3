89caf337d5434e479243ff099a18f336d1ba01d2
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-08-30 01:23:47 -0300

    Create Aula09 parte2.py

---
+++ Python Básico ( Lógica de Programação)/Aula09 parte2.py
@@ -0,0 +1,15 @@
+"""
+Entrada de dados - Aula 3
+"""
+#Soma
+numero_1= int(input('Digite um número: '))
+numero_2 =input('Digite outro número: ')
+numero_2 = int(numero_2)
+
+#Potencialização
+numero_3= int(input('Digite um número para potencializar: '))
+numero_4 =input('Digite outro número: ')
+numero_4 = int(numero_4)
+
+print( numero_1 + numero_2) #resultado da soma
+print( numero_3 ** numero_4) #resultado da potencialização
